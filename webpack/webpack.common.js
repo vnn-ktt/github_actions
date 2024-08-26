0f70819c5a9cb5c3d7d08ea0205d72944466f02b
Ivan Platunov <106499823+vnn-ktt@users.noreply.github.com>
2024-08-26 20:20:06 +0300

    Update webpack.common.js

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -9,11 +9,11 @@ const production = process.env.NODE_ENV === 'production';
 module.exports = {
 	entry: path.resolve(__dirname, '..', './src/index.tsx'), //точка входа в наше приложение содержит абсолютный путь к index.ts
 	output: {
-		path: path.resolve(__dirname, '..', './dist'), //путь куда будет собираться наш проект
+		path: path.resolve(__dirname, '..', './dist'), // путь, по которому будет собираться наш проект
 		filename: production
 			? 'static/scripts/[name].[contenthash].js'
 			: 'static/scripts/[name].js', // имя нашего бандла
-		publicPath: '/',
+		publicPath: process.env.PUBLIC_PATH ? process.env.PUBLIC_PATH : '/', // указываем путь, который будет добавляться перед подключением файлов
 		chunkFilename: 'static/scripts/[name].[contenthash].bundle.js'
 	},
 	//Нужно помочь вебпаку научится работать с jsx и tsx файлами для этого используют ts loader
@@ -79,6 +79,12 @@ module.exports = {
 		extensions: ['.js', '.jsx', '.tsx', '.ts', '.json'], //указываем файлы с которыми будет работать webpack
 	},
 	plugins: [
+		plugins: [
+		new webpack.EnvironmentPlugin({
+		            PUBLIC_PATH: null, // значение по умолчанию null, если переменная process.env.PUBLIC_PATH не передана
+		            NODE_ENV: 'development', // значение по умолчанию 'development', если переменная process.env.NODE_ENV не передана
+		        }),
+		],
 		new HTMLWebpackPlugins({
 			template: path.resolve(__dirname, '..', './public/index.html'),
 		}),
